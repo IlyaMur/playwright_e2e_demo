bad04705a730253775726f06c65b3e0692006f94
IlyaMur <IlyaMur@users.noreply.github.com>
2025-03-01 11:16:41 +0000

    deploy: 373b5477bce9a4543dc57c769d4a58b8af3b5551

--- fixtures/contexts/types/pageObjectContext.fixture.ts
+++
@@ -1,6 +0,0 @@
-import { Page } from '@playwright/test';
-import { CommonUtilsFixture } from '../../utils/common.fixture';
-
-export type PageContextFixture = CommonUtilsFixture & {
-  contextPage: Page;
-};
